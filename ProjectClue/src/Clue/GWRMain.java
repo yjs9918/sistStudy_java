2b78f1fb642d1fa4c081223ca6239fa97bb01b60
yjs9918 <yjs9918@naver.com>
2016-01-28 15:52:34 +0900

    게임 대기실,로그인

--- ProjectClue/src/Clue/GWRMain.java
+++ ProjectClue/src/Clue/GWRMain.java
@@ -5,15 +5,17 @@ import java.awt.event.*;
 public class GWRMain extends JFrame implements ActionListener{
 	CardLayout card=new CardLayout();
 	GameWaitingRoom gwr=new GameWaitingRoom();
-	
+	Login login=new Login();
 	public GWRMain()
 	{
 		setLayout(card);
-		add("gwr",gwr);
+		add("LOG",login);
+		add("GWR",gwr);
 		setSize(1200,900);
 		setVisible(true);
 		setResizable(false);
 		
+		login.b1.addActionListener(this);
 		gwr.tf.addActionListener(this);
 	}
 	public static void main(String[] args) {
@@ -29,7 +31,11 @@ public class GWRMain extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource()==gwr.tf)
+		if(e.getSource()==login.b1)
+		{
+			card.show(getContentPane(),"GWR");
+		}
+		else if(e.getSource()==gwr.tf)
 		{
 			String data= gwr.tf.getText();
 			gwr.chat.append(data+"\n");
