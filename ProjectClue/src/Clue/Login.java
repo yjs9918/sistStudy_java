eeb44614374577a62e8fa1f95f3583b5181bad27
yjs9918 <yjs9918@naver.com>
2016-02-01 19:23:12 +0900

    login update

--- ProjectClue/src/Clue/Login.java
+++ ProjectClue/src/Clue/Login.java
@@ -28,20 +28,20 @@ public class Login extends JPanel{
 		tf=new JTextField();
 		pf=new JPasswordField();
 		b1=new JButton("로그인");
-		b2=new JButton("취소");
+		b2=new JButton("회원가입");
 		
 		JPanel p=new JPanel(); // FlowLayout
 		p.add(b1);
 		p.add(b2);
-		p.setBounds(520, 480, 150, 35);
+		p.setBounds(520, 555, 150, 35);
 		p.setOpaque(false);
 		// 배치
 		//setLayout(new GridLayout(3, 2 , 5 , 5)); //뒤쪽 5(좌우간격),5(위아래간격)
 		setLayout(null); //사용자 지정
-		la1.setBounds(490, 400, 30, 30);
-		tf.setBounds(520, 400, 150, 30);
-		la2.setBounds(490, 435, 30, 30);
-		pf.setBounds(520, 435, 150, 30);
+		la1.setBounds(490, 480, 30, 30);
+		tf.setBounds(520, 480, 150, 30);
+		la2.setBounds(490, 515, 30, 30);
+		pf.setBounds(520, 515, 150, 30);
 		// 추가
 		add(la1); add(tf);
 		add(la2); add(pf);
