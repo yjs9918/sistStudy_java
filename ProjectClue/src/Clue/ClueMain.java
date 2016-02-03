88d822900407258b6f1881919cc25f2727a7d72e
kyungeun kim <kyungeunni@gmail.com>
2016-02-03 15:22:06 +0900

    mm

--- ProjectClue/src/Clue/ClueMain.java
+++ ProjectClue/src/Clue/ClueMain.java
@@ -4,13 +4,12 @@ import javax.swing.*;
 
 import java.awt.event.*;
 
-
-public class ClueMain extends JFrame implements ActionListener{
+public class ClueMain extends JFrame implements ActionListener,KeyListener{
 	CardLayout card;
 	GameWaitingRoom gwr=new GameWaitingRoom();
 	Login login=new Login();
 	GameMainScreen mainScreen=new GameMainScreen();
-	
+
 	
 	
 	public ClueMain()
@@ -20,6 +19,7 @@ public class ClueMain extends JFrame implements ActionListener{
 		add("LOG",login);
 		add("GWR",gwr);
 		add("MS",mainScreen);
+		
 		setSize(1200,900);
 		setVisible(true);
 		setResizable(false);
@@ -28,9 +28,9 @@ public class ClueMain extends JFrame implements ActionListener{
 		gwr.chatInput.addActionListener(this);
 		gwr.btnReady.addActionListener(this);
 		gwr.btnExit.addActionListener(this);
-		
-		mainScreen.addKeyListener(mainScreen);
-		mainScreen.setFocusable(true);
+
+		addKeyListener(this);
+
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -39,8 +39,7 @@ public class ClueMain extends JFrame implements ActionListener{
 			UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
 		}catch(Exception ex){}
 		ClueMain mn=new ClueMain();
-		
-		
+
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -57,14 +56,34 @@ public class ClueMain extends JFrame implements ActionListener{
 		}
 		else if(e.getSource()==gwr.btnReady)
 		{	
+
 			repaint();
-			
+			//서버에 준비완료 알려주기
 			card.show(getContentPane(), "MS");
+			//게임시작
+			
+			
 		}else if(e.getSource()==gwr.btnExit){
 			repaint();
 			card.previous(getContentPane());
 		}
 	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	
 	
 
