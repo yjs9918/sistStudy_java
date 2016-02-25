5ea9f276a8cbd377f7164db035ed58308b4b71a1
yjs9918 <yjs9918@naver.com>
2016-02-26 08:57:55 +0900

    wav 추가

--- ProjectClue/src/Clue/ShowTurn.java
+++ ProjectClue/src/Clue/ShowTurn.java
@@ -2,6 +2,7 @@ package Clue;
 
 
 
+import java.awt.Image;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -13,10 +14,12 @@ public class ShowTurn extends JFrame {
 	JLabel la1;
 	JLabel label ;
 	JLabel label2 ;
+	JPanel p1;
 
 	public JButton b1;
 	public ShowTurn(){
-		
+		JPanel p1= new JPanel();
+		p1.add("Center", new JLabel(new ImageIcon(setImage("image/showturn.jpg", p1.getWidth(), p1.getHeight()))));
 		setSize(300,300);
 		
 		label = new JLabel("MY TURN ");
@@ -43,6 +46,12 @@ public void setLabel(String id, int dice){
 		label2.setText("ÀÌµ¿È½¼ö: "+dice);
 		
 	}*/
-	
+	private Image setImage(String filename, int width, int height) {
+		// TODO Auto-generated method stub
+		ImageIcon ii = new ImageIcon(filename);
+		Image image = ii.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		return image;
+		// return null;
+	}
 	
 }
